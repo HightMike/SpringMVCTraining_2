ca6ba3836047f71658d1328894cd5bba1982d450
HightMike <baobab-borisov@mail.ru>
2019-05-29 23:57:56 +0300

    Создание структуры

---
+++ src/main/java/MainController.java
@@ -0,0 +1,17 @@
+import org.springframework.cglib.core.Local;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Locale;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(Locale locale, Model model) {
+        return "index";
+    }
+
+}
